f5a58633393c0f507ac8f6dd094165463f3bebfd
David Edward Weirich <weirich.david@gmail.com>
2016-12-30 09:30:14 -0500

    Made cube be inside unit sphere

--- include/assets.h
+++ include/assets.h
@@ -1,43 +1,28 @@
 #ifndef ASSETS_H
 #define ASSETS_H
 
+/*
+ *  Just some basic assets which can be used to test out 2D/3D graphics.
+ *  All platonic solids are centered at the origin and have vertices on the unit shpere.
+ */
+
+#define A 0.47140452f // = sqrt(2)/3
+
 static const GLfloat cube_vertex_data[] = {
-    -1.0f,-1.0f,-1.0f, 
-    -1.0f,-1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f, 
-    1.0f, 1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f,-1.0f, 
-    1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    -1.0f,-1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f,-1.0f,
-    -1.0f, 1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f,-1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f
+     -A, -A, -A, -A, -A,  A, -A,  A,  A,
+      A,  A, -A, -A, -A, -A, -A,  A, -A,
+      A, -A,  A, -A, -A, -A,  A, -A, -A,
+      A,  A, -A,  A, -A, -A, -A, -A, -A,
+     -A, -A, -A, -A,  A,  A, -A,  A, -A,
+      A, -A,  A, -A, -A,  A, -A, -A, -A,
+     -A,  A,  A, -A, -A,  A,  A, -A,  A,
+      A,  A,  A,  A, -A, -A,  A,  A, -A,
+      A, -A, -A,  A,  A,  A,  A, -A,  A,
+      A,  A,  A,  A,  A, -A, -A,  A, -A,
+      A,  A,  A, -A,  A, -A, -A,  A,  A,
+      A,  A,  A, -A,  A,  A,  A, -A,  A
 };
+
+#undef A
 
 #endif
