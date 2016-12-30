41cc641e2aa0ef7746fefe4779c6870ddaeb6167
David Edward Weirich <weirich.david@gmail.com>
2016-12-29 22:28:39 -0500

    Added failing test

--- test/minus/minus_test.c
+++ test/minus/minus_test.c
@@ -10,6 +10,12 @@ START_TEST(five_minus_two_equals_three)
 }
 END_TEST
 
+START_TEST(a_failing_test)
+{
+    ck_assert_int_eq(0, 1);
+}
+END_TEST
+
 Suite *make_minus_suite()
 {
     Suite *s; 
@@ -19,6 +25,7 @@ Suite *make_minus_suite()
     tc = tcase_create("Core");
 
     tcase_add_test(tc, five_minus_two_equals_three);
+    tcase_add_test(tc, a_failing_test);
 
     suite_add_tcase(s, tc);
 
