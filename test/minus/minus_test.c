a85b4b26fce04a2bd9912aaccda3dbf63713fa33
David Edward Weirich <weirich.david@gmail.com>
2016-12-29 22:34:26 -0500

    Removed the failing test, verified that travis fails with it on

--- test/minus/minus_test.c
+++ test/minus/minus_test.c
@@ -10,11 +10,13 @@ START_TEST(five_minus_two_equals_three)
 }
 END_TEST
 
+#if 0
 START_TEST(a_failing_test)
 {
     ck_assert_int_eq(0, 1);
 }
 END_TEST
+#endif
 
 Suite *make_minus_suite()
 {
@@ -25,8 +27,9 @@ Suite *make_minus_suite()
     tc = tcase_create("Core");
 
     tcase_add_test(tc, five_minus_two_equals_three);
+#if 0
     tcase_add_test(tc, a_failing_test);
-
+#endif
     suite_add_tcase(s, tc);
 
     return s;
